221e03d7a362f463bf4c74864cb6f6a52da0aace
koksek12 <92683017+koksek12@users.noreply.github.com>
2023-11-09 16:16:49 +0100

    Create Discord_Bot_Api.py
    Jest to bot wysyłający losowe obrazki

---
+++ Discord_Bot_Api.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+    
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Po wywołaniu polecenia duck program wywołuje funkcję get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command('dog')
+async def dog(ctx):
+    '''Po wywołaniu polecenia duck program wywołuje funkcję get_duck_image_url'''
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
+
+bot.run("MTE2ODEwMTc5NzA5MzcwNzg1Nw.Ghdn5k.P8aRYXslhjPww_ckEmvt3Sey4RCfU0oG0v7fMk")
